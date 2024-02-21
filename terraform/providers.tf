1c9186c54c4f610ff9e25f781283331b41c78e6e
Mohammad Parvizi <rezaparvizi1994@gmail.com>
2024-02-21 11:50:57 -0500

    fix: upgrade aws provider version

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,3 +1,20 @@
+terraform {
+  backend "s3" {
+    bucket                 = "edgecom-prod-tf-states"
+    key                    = "terraforms/tdengine-cluster/terraform.tfstate"
+    region                 = "ca-central-1"
+    skip_region_validation = true
+    profile                = "edgecom-prod"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.37.0"
+    }
+  }
+}
+
 provider "aws" {
   region  = var.region
   profile = var.profile
