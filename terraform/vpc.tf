75cfa7e629c9c873ab08e8ab620ebce75de2a21d
Mohammad Parvizi <rezaparvizi1994@gmail.com>
2024-02-22 15:08:55 -0500

    fix: add general vpc

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,18 +1,52 @@
-data "aws_vpc" "vpc" {
-  cidr_block = "10.0.0.0/16"
+# Create a new VPC
+resource "aws_vpc" "vpc" {
+  cidr_block       = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
 
   tags = {
+    Name = "MyVPC"
     Terraform = "true"
   }
 }
 
-data "aws_subnets" "public" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.vpc.id]
+# Create a public subnet within the VPC
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "MyPublicSubnet"
+    Public = "true"
+  }
+}
+
+# Internet Gateway for the VPC
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "MyInternetGateway"
+  }
+}
+
+# Route table for the public subnet
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
   }
 
   tags = {
-    Public = "true"
+    Name = "MyPublicRoutes"
   }
+}
+
+# Associate the route table with the subnet
+resource "aws_route_table_association" "rta" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
 }
